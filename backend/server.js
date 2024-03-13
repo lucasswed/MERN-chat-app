503371878d914c20909741065f6b29c1868de6bc
wslHome <lucas@Lucas-Desktop>
2024-03-13 17:11:45 +0000

    random commit

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,26 @@
+import cookieParser from "cookie-parser";
 import "dotenv/config";
 import express from "express";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
+  connectToMongoDB();
   console.log(`Server Running on Port ${PORT}`);
   console.log(`Access it through http://localhost:${PORT}`);
 });
-
-// 22:35
