7ca806886c0cc13cc0b29ab10466d78085efc35f
wslHome <lucas@Lucas-Desktop>
2024-03-20 11:25:39 +0000

    finnished the beackend with the getUsers done

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,11 @@ import cookieParser from "cookie-parser";
 import "dotenv/config";
 import express from "express";
 
-import connectToMongoDB from "./db/connectToMongoDB.js";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -18,6 +20,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDB();
