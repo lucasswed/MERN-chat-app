9655fe3f1c18bd0ba8bcbd8bbf76706ff76e046e
wslHome <lucas@Lucas-Desktop>
2024-03-26 11:42:23 +0000

    fix: sound of new message play even when in another conversation and added a toaster

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -2,22 +2,36 @@ import { useEffect } from "react";
 import { useSocketContext } from "../context/SocketContext";
 import useConversation from "../zustand/useConversation";
 
+import toast from "react-hot-toast";
 import notificationSound from "../assets/sounds/notification.mp3";
 
 const useListenMessages = () => {
   const { socket } = useSocketContext();
-  const { messages, setMessages } = useConversation();
+  const { messages, setMessages, selectedConversation } = useConversation();
 
   useEffect(() => {
     socket?.on("newMessage", (newMessage) => {
+      if (
+        !selectedConversation ||
+        newMessage.senderId !== selectedConversation._id
+      ) {
+        const sound = new Audio(notificationSound);
+        sound
+          .play()
+          .catch((error) => {})
+          .finally(() => toast("New message", { icon: "🔔" }));
+        return;
+      }
       newMessage.shouldShake = true;
       const sound = new Audio(notificationSound);
       sound.play();
+      toast("New message", { icon: "🔔" });
+
       setMessages([...messages, newMessage]);
     });
 
     return () => socket?.off("newMessage");
-  }, [socket, setMessages, messages]);
+  }, [socket, setMessages, messages, selectedConversation]);
 };
 
 export default useListenMessages;
